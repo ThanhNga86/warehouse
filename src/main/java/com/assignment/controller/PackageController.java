088351b3e2868a20d46ee12f7fc08c7f5d945af6
Thanh Ngà <testtest86n@gmail.com>
2024-01-30 14:59:53 +0700

    file

--- src/main/java/com/assignment/controller/PackageController.java
+++ src/main/java/com/assignment/controller/PackageController.java
@@ -1,13 +1,26 @@
 package com.assignment.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.assignment.entity.WareHouse;
+import com.assignment.repo.WareHouseRepository;
 
 @Controller
 public class PackageController {
+	@Autowired
+	WareHouseRepository Warehousedao;
 
 	@GetMapping("/package/addPackage")
-	public String addPackage() {
+	public String addPackage(Model model) {
+		WareHouse WHitem = new WareHouse();
+		model.addAttribute("WHitem", WHitem);
+		List<WareHouse> WHitems = Warehousedao.findAll();
+		model.addAttribute("WHitems", WHitems);
 		return "admin/package/package";
 	}
 	
