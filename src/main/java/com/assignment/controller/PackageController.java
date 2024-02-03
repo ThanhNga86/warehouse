ad89dfd0c6303d305a03b64b665e28515d5968a3
Thanh Ngà <testtest86n@gmail.com>
2024-02-03 14:56:19 +0700

    file

--- src/main/java/com/assignment/controller/PackageController.java
+++ src/main/java/com/assignment/controller/PackageController.java
@@ -1,23 +1,31 @@
 package com.assignment.controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.assignment.DAO.PackageDAO;
+import com.assignment.entity.Package;
+import com.assignment.entity.WareHouse;
 
 @Controller
 public class PackageController {
+	@Autowired
+	private PackageDAO packageDAO;
 
 	@GetMapping("/package/addPackage")
-<<<<<<< HEAD
 	public String addPackage(Model model) {
 		WareHouse WHitem = new WareHouse();
 		model.addAttribute("WHitem", WHitem);
-		List<WareHouse> WHitems = Warehousedao.findAll();
+		List<Package> WHitems = packageDAO.findAll();
 		model.addAttribute("WHitems", WHitems);
 		return "/admin/package/package";
-=======
+	}
+
 	public String addPackage() {
 		return "admin/package/package";
->>>>>>> b2a46965f2cd2b1e80d029987062bf7d16a3a343
 	}
 }
