383f7b7e616a2b2a9cdcc772bd28d7cd46422339
chiencycole <chiengauden@gmail.com>
2024-02-22 15:25:47 +0700

    chien

--- src/main/java/com/assignment/entity/Traffic.java
+++ src/main/java/com/assignment/entity/Traffic.java
@@ -3,11 +3,8 @@ package com.assignment.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-<<<<<<< HEAD
 import jakarta.persistence.CascadeType;
-=======
 import jakarta.persistence.Column;
->>>>>>> 67fcfaa427c88e103d7a1789a56429f8ee3fb991
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
